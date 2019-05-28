b0981cbb26de3e4cdd3f24b02dcb3ba33b92bdbb
henriquesd <hsd.foz@gmail.com>
2019-05-28 14:14:36 -0300

    Added DB configuration

--- Startup.cs
+++ Startup.cs
@@ -2,20 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookApiProject.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BookApiProject
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Using SQL \/;
+            // var connectionString = Configuration["connectionStrings:bookDbConnectionString"];
+            // services.AddDbContext<BookDbContext>(c => c.UseSqlServer(connectionString));
+            // then add this line on appsettings.json, on "connectionStrings": "bookDbConnectionString": "Server=(localdb)\\mssqllocaldb;Database=BookApiProject;Trusted_Connection=True;"
+            
+            services.AddDbContext<BookDbContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
